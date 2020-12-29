85f73d3d7caa32f40169feecd075dae367e710c9
Davidohis <davidi.ohis25@gmail.com>
2020-12-29 03:00:54 +0000

    testing router

---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './navbar.css';
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className='nav'>  
+                <span>Logo</span>
+                <nav>
+                    <Link className='link' to='/'>
+                        <li>Home</li>
+                    </Link>
+                    <Link className='link' to='/about'>
+                        <li>About</li>
+                    </Link>
+                    <Link className='link' to='/shop'>
+                        <li>Shop</li>
+                    </Link>
+                </nav>
+            </div>
+        </div>
+    )
+}
